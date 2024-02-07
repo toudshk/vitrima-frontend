173e3421cf66583ddb20b89a3173d1156e3c0f26
Ivan Troshin <mxsdbsx@gmail.com>
2024-02-07 21:19:58 +1000

    change first page

--- src/components/screens/first-page/onboard-cards/OnboardCards.tsx
+++ src/components/screens/first-page/onboard-cards/OnboardCards.tsx
@@ -1,28 +1,29 @@
 import Image from "next/image";
 import React from "react";
-import bigImg from "../../../common/images/ui/mainPage/bigImage.png";
-import smallImg from "../../../common/images/ui/mainPage/smallImage1.png";
-import smallImg2 from "../../../common/images/ui/mainPage/smallImage2.png";
-import bigImg2 from "../../../common/images/ui/mainPage/bigImage2.png";
+import bigImg from "../../../common/images/ui/mainPage/bigImage.jpg";
+import smallImg from "../../../common/images/ui/mainPage/smallImage1.jpg";
+import smallImg2 from "../../../common/images/ui/mainPage/smallImage2.jpg";
+import bigImg2 from "../../../common/images/ui/mainPage/bigImage2.jpg";
 import styles from "./OnboardCards.module.scss";
-import smallImg3 from "../../../common/images/ui/mainPage/smallImage3.png";
-import smallImg4 from "../../../common/images/ui/mainPage/smallImage4.png";
+import smallImg3 from "../../../common/images/ui/mainPage/smallImage3.jpg";
+import smallImg4 from "../../../common/images/ui/mainPage/smallImage4.jpg";
 const OnboardCards = () => {
   return (
     <div className='max-w-[1736px] mx-auto'>
       <div className={styles.content}>
         <div className={styles.images}>
-          <Image src={bigImg} height={960} width={472} alt={""} />
+         
           <div className={styles.smallImages}>
             <Image
               src={smallImg}
-              height={472}
-              width={472}
+              height={245}
+              width={245}
               alt={""}
               
             />
-            <Image src={smallImg2} height={472} width={472} alt={""} />
+            <Image src={smallImg2} height={245} width={245} alt={""} />
           </div>
+          <Image src={bigImg} height={960} width={472} alt={""} />
         </div>
         <div className={styles.text}>
           <h1>Подрядчики</h1>
@@ -55,11 +56,11 @@ const OnboardCards = () => {
             <Image
            
               src={smallImg3}
-              height={472}
-              width={472}
+              height={245}
+              width={245}
               alt={""}
             />
-            <Image src={smallImg4} height={472} width={472} alt={""} />
+            <Image src={smallImg4} height={245} width={245} alt={""} />
           </div>
           <Image src={bigImg2} height={960} width={472} alt={""} />
         </div>
