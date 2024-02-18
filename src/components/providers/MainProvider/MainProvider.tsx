d320903266b204130a34c2bcc4d16903b6eb1f49
Ivan Troshin <mxsdbsx@gmail.com>
2024-02-18 15:22:10 +1000

    fix

--- src/components/providers/MainProvider/MainProvider.tsx
+++ src/components/providers/MainProvider/MainProvider.tsx
@@ -7,17 +7,35 @@ import { ReduxProvider } from "../ReduxProvider";
 import Header from "@/components/layout/header/Header";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { ToastContainer } from "react-toastify";
+import { motion } from "framer-motion";
+const animation = {
+  hidden: {
+  
+    opacity: 0,
+  },
+  visible: {
+  
+    opacity: 1,
+  },
+};
 
 // eslint-disable-next-line react-hooks/rules-of-hooks
 const queryClient = new QueryClient();
 
-const MainProvider: FC<{children: any}> = ({ children }) => {
+const MainProvider: FC<{ children: any }> = ({ children }) => {
   return (
     <QueryClientProvider client={queryClient}>
-      
       <ReduxProvider>
         <Header />
-         <div className="mt-[7vh]">{children}</div>
+        <motion.div
+          className="mt-[7vh]"
+          variants={animation}
+          initial="hidden"
+          animate="visible"
+          exit="hidden"
+        >
+          {children}
+        </motion.div>
       </ReduxProvider>
       <ToastContainer />
     </QueryClientProvider>
