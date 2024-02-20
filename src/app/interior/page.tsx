0a8d83a9c385859d2fc5bbc688b3b583a189d090
Ivan Troshin <mxsdbsx@gmail.com>
2024-02-20 11:39:25 +1000

    add redirect for not auth users

--- src/app/interior/page.tsx
+++ src/app/interior/page.tsx
@@ -1,20 +1,22 @@
-"use client"
-import React from "react";
+"use client";
+import React, { useLayoutEffect } from "react";
 import styles from "./page.module.scss";
 
 import InteriorGallery from "@/components/screens/main-page/Gallery";
-
+import { useAuth } from "@/hooks/useAuth";
+import { redirect } from "next/navigation";
 
 export default function Page() {
- 
+  const { user } = useAuth();
+  if (!user) {
+    redirect("/");
+  }
   return (
     <>
-        <div className={styles.container}>
-          <div className={styles.title}>Интерьер</div>
-          <InteriorGallery   slug={'interior'}/>
-        </div>
-     
+      <div className={styles.container}>
+        <div className={styles.title}>Интерьер</div>
+        <InteriorGallery slug={"interior"} />
+      </div>
     </>
   );
 }
-
--- src/components/shared/types/auth.types.ts
+++ src/components/shared/types/auth.types.ts
@@ -1,12 +1,15 @@
-import { NextPage } from 'next'
-import { ReactNode } from 'react';
+import { NextPage } from "next";
+import { ReactNode } from "react";
 
-export type TypeRoles = { isOnlyAdmin?: boolean; isOnlyContractor?: boolean }
+export type TypeRoles = { isOnlyAdmin?: boolean; isOnlyContractor?: boolean };
 
-export type NextPageAuth<P = {}> = NextPage<P> & TypeRoles
+export type NextPageAuth<P = {}> = NextPage<P> & TypeRoles;
 
-export type TypeComponentAuthFields = {   children: React.ReactNode;
-    Component: {
-      isOnlyAdmin: boolean;
-      isOnlyContractor: boolean;} }
-// Component: TypeRoles, 
+export type TypeComponentAuthFields = {
+  children: React.ReactNode;
+  Component: {
+    isOnlyAdmin: boolean;
+    isOnlyContractor: boolean;
+  };
+};
+// Component: TypeRoles,
