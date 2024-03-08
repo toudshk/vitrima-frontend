aa3b8b1257545f02e2730af73faf5c5e37f6febf
stepintotha6ix <mxsdbsx@gmail.com>
2024-03-08 18:37:06 +1000

    fix socket

--- src/api/socket.ts
+++ src/api/socket.ts
@@ -4,7 +4,7 @@ class SocketApi {
   static socket: null | Socket = null;
   static createConnection() {
     // Используйте https вместо http, если ваш сервер поддерживает HTTPS
-    this.socket = io(`${process.env.APP_URL}`, {
+    this.socket = io('wss://vitrima.su', {
       secure: true, // Это обычно необходимо для работы с HTTPS
       rejectUnauthorized: false, // Это может быть необходимо для самоподписанных сертификатов, но будьте осторожны
     });
