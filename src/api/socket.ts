a3255b293b90616111939884f967553b813ee5e0
stepintotha6ix <mxsdbsx@gmail.com>
2024-03-08 18:27:01 +1000

    fix port

--- src/api/socket.ts
+++ src/api/socket.ts
@@ -2,17 +2,20 @@ import io, { Socket } from "socket.io-client";
 
 class SocketApi {
   static socket: null | Socket = null;
-
   static createConnection() {
-    this.socket = io(`${process.env.APP_URL}`)
+    // Используйте https вместо http, если ваш сервер поддерживает HTTPS
+    this.socket = io(`${process.env.APP_URL}`, {
+      secure: true, // Это обычно необходимо для работы с HTTPS
+      rejectUnauthorized: false, // Это может быть необходимо для самоподписанных сертификатов, но будьте осторожны
+    });
 
     this.socket.on("connect", () => {
       console.log("connected");
     });
+
     this.socket.on("disconnect", (e) => {
       console.log("disconnect");
     });
-    
   }
 }
 export default SocketApi
