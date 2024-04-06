40d8737c48b2f2b874f5d4086c8c1bc48740e83c
stepintotha6ix <mxsdbsx@gmail.com>
2024-04-06 19:55:32 +1000

    fix bugs

--- src/components/screens/profile/Profile.tsx
+++ src/components/screens/profile/Profile.tsx
@@ -6,20 +6,21 @@ import { useAuth } from "@/hooks/useAuth";
 
 interface ProfileProps {
   data: any;
-  isLoading: boolean
+  isLoading: boolean;
 }
 
 const Profile: React.FC<ProfileProps> = ({ data, isLoading }) => {
-const {user} = useAuth()
-
-  if (user?.isContractor) {
-    return <ContractorProfile userData={data} id={data?._id} isLoading={isLoading} />;
   
-  } else {
-    return <ApplicantProfile data={data} id={data?._id} isLoading={isLoading} />;
+    return (
+      <>
+      {data?.isContractor ? (
+        <ContractorProfile userData={data} id={data?._id} isLoading={isLoading} />
+      ) : (
+        <ApplicantProfile data={data} id={data?._id} isLoading={isLoading} />
+      )}
+    </>
+    );
 
-  }
 };
 
 export default Profile;
-
