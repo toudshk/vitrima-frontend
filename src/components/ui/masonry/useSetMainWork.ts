0c955be04268ec8b5c0956a6028fc7667547d0c7
stepintotha6ix <mxsdbsx@gmail.com>
2024-05-05 20:18:12 +1000

    fix

--- src/components/ui/masonry/useSetMainWork.ts
+++ src/components/ui/masonry/useSetMainWork.ts
@@ -10,12 +10,12 @@ import { toast } from "react-toastify";
 
 export const useSetMainWork = (id: any, item: any) => {
   
-  const { data, isLoading } = useWork();
-  
-  const mainWorksCount = data?.filter((work) => work.isMainWork).length;
+  const { data } = useWork();
+  const mainWorksCount = data?.filter((work) => work.isMainWork).length ?? 0;
+
   console.log(mainWorksCount)
   const { mutateAsync } = useMutation("work", async () => {
-    if ((mainWorksCount && mainWorksCount < 2) || item.isMainWork === true) {
+    if ((mainWorksCount < 2) || item.isMainWork === true) {
       await WorkService.setMainWork(id, item._id);
       window.location.reload();
     }
